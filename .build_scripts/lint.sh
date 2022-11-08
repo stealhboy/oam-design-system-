2b552055bde540b1417a102f862edb5b60e0e93e
stealhboy <117743957+stealhboy@users.noreply.github.com>
2022-11-08 22:13:17 +0800

    Update lint.sh

--- .build_scripts/lint.sh
+++ .build_scripts/lint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-set -e # halt script on error
-# If deploying, don't balk on lint errors
+set -e # 停止腳本錯誤
+# If deploying, don't balk on lint errors 如果成功執行,不要猶豫在lint 的錯誤上
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
   npm run lint || true
 else
